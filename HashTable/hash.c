d66fe08dd7566be078f325e6a992aee89e2b61dc
Agnieszka Maule <Agnieszka Maule>
2017-04-04 08:47:53 -0300

    code scope for implementing a Hash table in C before implemented in Assembly

---
+++ HashTable/hash.c
@@ -0,0 +1,27 @@
+
+#define tamanho = 16
+
+int funcHash(int valor){
+  return valor%tamanho;
+}
+
+void insercao(int valor, ){
+
+}
+
+void remocao(int valor, ){
+
+}
+
+int busca(int valor, ){
+  return valor; // valor esta na hash
+  return -1; // valor nao esta na hash
+}
+
+void printHash(){
+
+}
+
+void main(){
+
+}
