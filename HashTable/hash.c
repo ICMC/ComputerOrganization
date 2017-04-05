9fee88993cb2d92f14d9b2796aa73bb6a90d91d4
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-05 00:28:28 -0300

    insert implemented

--- HashTable/hash.c
+++ HashTable/hash.c
@@ -1,6 +1,59 @@
 
+
+#include <stdio.h>
+#include <stdlib.h>
 #define hashSize = 16
 
+// Implement doubly linked list
+//
+struct node{
+  struct node *prev;
+  int n;
+  struct node *next;
+};
+
+int hashFunc(int value){
+  return value%hashSize;
+}
+
+struct *node createNode(int value){
+  struct node * newNode;
+  newNode = (struct *node)malloc(1*sizeof(struct *node));
+  newNode->prev = NULL;
+  newNode->next = NULL;
+  newNode->n = value;
+  return newNode;
+}
+
+void insertBegining(int value, struct node *hash; int *sizeVector, int index){
+    struct node* newNode = createNode(value);
+    // insert begining
+    if(hash[index][0] == NULL){
+      hash[index][0] = newNode;
+
+    }
+    else{
+      hash[index][0] -> prev = newNode;
+      newNode-> next = hash[index][0];
+      hash[index][0] = newNode;
+    }
+    sizeVector[index]++;
+}
+
+// insert any middle
+void insertMiddle(int value, struct node *hash, int *sizeVector, int index){
+    struct node*  newNode = createNode(value);
+    
+}
+
+
+else if(value > hash[index][0] ->n && value < hash[index][sizeVector[index] - 1] -> n){
+
+}
+// insert end
+else if(value >=  hash[index][sizeVector[index] - 1]-> n ){
+
+}
 void menu(int opcao){
   printf("1.Insertion \n");
   printf("2.Remove \n");
@@ -11,24 +64,7 @@ void menu(int opcao){
 void sort(){
 
 }
-int funcHash(int value){
-  return value%hashSize;
-}
 
-void insert(int value, int **hash, int *sizeVector){
-    int index =  funcHash(value);
-    if(hash[index][0] == NULL){ // if there is nothing on the array, allocate 1 space for it
-      hash[index] = malloc(1*sizeof(int));
-      hash[index][0] = value; // insert first value into the first position allocated
-      sizeVector[index] = 1;
-    }else if(hash[index]){ //if there are already values on the array, reallocate more memory
-      hash[index]=realloc(hash[index], (sizeVector[index]+1)*sizeof(int));
-      hash[index][sizeVecto[index]] = value;
-      sort();
-      sizeVector++;
-    }
-
-}
 
 void remove(int value, int **hash){
     int index = funcHash(value);
@@ -46,14 +82,11 @@ void printHash(){
 
 void main(){
 
-  int *hash[16];// vector of 16 pointers
+  temp = left = right = NULL;
+  struct node *hash[16];// vector of 16 pointers to node structures
   int  *sizeVector; // vector that will keep track of the doubly linked lists size
   int option, value;
   sizeVector = (int*) calloc(16, sizeof(int));
-
-  for(int i=0; i < 16 ; i++){
-    hash[i] = malloc(1*sizeof(int));
-  }
 
   menu(option);
 
