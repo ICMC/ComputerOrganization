f8fabe97c2d17e5557aebb8fdce30bff738288b1
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-06 20:45:21 -0300

    implemented function to print all the hash and debbuged majority of the code

--- HashTable/hash.c
+++ HashTable/hash.c
@@ -13,6 +13,8 @@ struct node{
 
 int hashFunc(int value){
   int hashSize = 16;
+  int result = value%hashSize;
+  printf("mod of value and hashSize = %d \n", result);
   return value%hashSize;
 }
 
@@ -25,26 +27,60 @@ struct node* createNode(int value){
   return newNode;
 }
 
+// binary search...
+int search(int value, struct node **hash, int index ){
+  struct node* aux =  hash[index];
+  if(aux == NULL){
+    return -1;
+  }
+  while(aux->n != value){
+      if(aux->next == NULL){
+          return -1; // value is not on the hash
+      }
+      aux = aux->next;
+  }
+  return value; // value is on the Hash
+}
+
 // insert nodes at the doubly ordered list
 void insert(int value, struct node **hash, int *sizeVector, int index){
     struct node*  newNode = createNode(value);
     struct node* aux = hash[index];
+    struct node *aux2;
 
-    if(hash[index] == NULL){ // if there is no node on the doubly list just insert it
-      hash[index] = newNode;
-      printf("value %d was inserted. \n", value);
+    int exist = search(value, hash, index); // checks if the key is already on the hash
+
+    if(exist == -1){ // if key is not on the hash then it will insert it
+      if(hash[index] == NULL){ // if there is no node on the doubly list just insert it
+        hash[index] = newNode;
+      }
+      else{
+          while((aux->n < value) && (aux->next != NULL)){ // while node value is less then the value to be inserted
+            aux = aux->next;
+          }
+          if(aux->prev == NULL){    // if the node is being inserted in the begining of the list
+            hash[index] = newNode;
+            newNode->next = aux;
+            aux->prev = newNode;
+          }
+          else if(aux->next == NULL && (aux->n < value)){ // if the node is the last one on the list
+            newNode->next = NULL;
+            newNode->prev = aux;
+            aux->next = newNode;
+          }
+          else{
+            aux2 = aux->prev;
+            aux2->next = newNode;
+            newNode->next = aux;
+            newNode->prev = aux->prev;
+            aux->prev = newNode;
+          }
+      }
+      sizeVector++;
+    }else{
+      printf("Key already exists on the Hash! \n");
     }
 
-    else{
-        while(aux->n < value){ // while node value is less then the value to be inserted
-          aux = aux->next;
-        }
-        newNode->next = aux;
-        newNode->prev = aux->prev;
-        aux->prev = newNode;
-        printf("value %d was inserted. \n", value);
-    }
-    sizeVector++;
 }
 
 
@@ -52,7 +88,8 @@ void menu(int *opcao){
   printf("\n1.Insertion \n");
   printf("2.Remove \n");
   printf("3.Search \n");
-  printf("4.Exit\n");
+  printf("4.Print Hash\n");
+  printf("5.Exit\n");
   printf("What option: ");
   scanf("%d", opcao);
 }
@@ -74,50 +111,67 @@ int removeKey(int value, struct node **hash, int *sizeVector, int index){
         else{
           aux= aux->next;
         }
-        // when it finds the node with the value searched
-        if(aux->prev == NULL && (aux->next != NULL)){ // checks if the node to be removed is the first on the list
+      }
+      // When there is only one node on the list
+      if(aux->next == NULL && aux->prev == NULL){
+          hash[index] = NULL;
+        }
+      // when the node is the first on the list
+      else if(aux->prev == NULL && (aux->next != NULL)){ // checks if the node to be removed is the first on the list
             aux2 = aux->next;
-            aux2->prev = aux->prev;
-            hash[index] = aux2;
+            aux2->prev = NULL;
+            hash[index] = aux->next;
 
-        }else{ // if its not the first element then just remove it
+      }
+      // when the node to be removed is the last on the list
+      else if(aux->next == NULL){
+            aux2 = aux->prev;
+            aux2-> next = NULL;
+      }
+      // if the node is in the middle of the list
+      else{
           aux2 = aux->prev;
-          aux2->next = aux->next;
+          aux2->next= aux->next;
+          aux2 = aux->next;
+          aux2->prev = aux->prev;
           printf("value %d was removed.\n", value);
         }
       }
 
       sizeVector[index]--;
       return value;
-    }
 }
 
-// binary search...
-int search(int value, struct node **hash, int index ){
-  struct node* aux =  hash[index];
-  while(aux->n != value){
-      if(aux->next == NULL){
-          return -1; // value is not on the hash
+
+
+void printHash(struct node **hash){
+    struct node * aux;
+    int i;
+    for(i =0; i < 16; i++){
+      aux =  hash[i];
+      printf("hash[%d] = ", i);
+      if(aux == NULL){
+        printf("NULL \n");
       }
-      aux = aux->next;
-  }
-  return value; // value is on the Hash
-}
+      while(aux!=NULL){
+        printf("%d ", aux->n);
+        aux = aux->next;
+      }
 
-void printHash(){
-
+      printf("\n");
+    }
 }
 
 void main(){
 
-  struct node **hash=(struct node **)calloc(16,sizeof(struct node**));// vector of 16 pointers to node structures
+  struct node **hash=(struct node **)malloc(16*sizeof(struct node**));// vector of 16 pointers to node structures
   int  *sizeVector; // vector that will keep track of the doubly linked lists size
   int option, value, index, result;
   sizeVector = (int*) calloc(16, sizeof(int));
 
   menu(&option);
 
-  while(option!=4){
+  while(option!=5){
     if(option == 1){
       printf("What value you wanna insert: ");
       scanf("%d", &value);
@@ -143,7 +197,9 @@ void main(){
       }else{
           printf("Value %d was found \n", result);
       }
-
+    }
+    else if(option==4){
+      printHash(hash);
     }
 
     menu(&option);
