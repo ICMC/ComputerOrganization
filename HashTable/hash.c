6d2a9744741868d29c726f837ec5018124d3fe59
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-06 17:13:28 -0300

    debugging code

--- HashTable/hash.c
+++ HashTable/hash.c
@@ -1,7 +1,7 @@
 // bia
 #include <stdio.h>
 #include <stdlib.h>
-#define hashSize 16
+
 
 // Implement doubly linked list
 //
@@ -12,6 +12,7 @@ struct node{
 };
 
 int hashFunc(int value){
+  int hashSize = 16;
   return value%hashSize;
 }
 
@@ -25,12 +26,13 @@ struct node* createNode(int value){
 }
 
 // insert nodes at the doubly ordered list
-void insert(int value, struct node *hash, int *sizeVector, int index){
+void insert(int value, struct node **hash, int *sizeVector, int index){
     struct node*  newNode = createNode(value);
-    struct node aux = hash[index];
+    struct node* aux = hash[index];
 
     if(hash[index] == NULL){ // if there is no node on the doubly list just insert it
       hash[index] = newNode;
+      printf("value %d was inserted. \n", value);
     }
 
     else{
@@ -40,30 +42,33 @@ void insert(int value, struct node *hash, int *sizeVector, int index){
         newNode->next = aux;
         newNode->prev = aux->prev;
         aux->prev = newNode;
+        printf("value %d was inserted. \n", value);
     }
     sizeVector++;
 }
 
 
-void menu(int opcao){
+void menu(int *opcao){
   printf("1.Insertion \n");
   printf("2.Remove \n");
   printf("3.Search \n");
-  printf("4.Exit")
-  printf("What option: %d", &opcao);
+  printf("4.Exit\n");
+  printf("What option: ");
+  scanf("%d", opcao);
 }
 
 
-int remove(int value, int **hash, int *sizeVector){
-    int index = funcHash(value);
+int removeKey(int value, struct node **hash, int *sizeVector, int index){
     struct node* aux =  hash[index];
     struct node* aux2;
-    if(hash[index] == NULL){ // check if the doubly list is not empty
+    if(hash[index] == NULL){ // check if the doubly list is empty
+      printf("value %d does not exists in the hash. \n", value);
       return -1;
     }
     else{
       while(aux->n != value){ // while value on the node is different than value searched , go to the next node
         if(aux->next == NULL){ // if the next node is NULL , then there is not a node with the valued searched
+          printf("value %d does not exists on the hash. \n", value);
           return -1;
         }
         else{
@@ -72,6 +77,7 @@ int remove(int value, int **hash, int *sizeVector){
       }
       aux2 = aux->prev;
       aux2->next = aux->next;
+      printf("value %d was removed.\n", value);
       if (aux->next != NULL){ // check if the pointer for the next node is NULL
         aux = aux->next;
         aux->prev = aux2;
@@ -82,10 +88,17 @@ int remove(int value, int **hash, int *sizeVector){
 }
 
 // binary search...
-int search(int value, int **hash ){
-  int index = funcHash(value);
+int search(int value, struct node **hash, int index ){
+  struct node* aux =  hash[index];
+  struct node* aux2;
+
+  while(aux->n != value){
+      if(aux->next == NULL){
+          return -1; // value is not on the hash
+      }
+      aux = aux->next;
+  }
   return value; // value is on the Hash
-  return -1; // value is not on the hash
 }
 
 void printHash(){
@@ -94,32 +107,40 @@ void printHash(){
 
 void main(){
 
-  temp = left = right = NULL;
-  struct node **hash=(struct node **)calloc(16,sizeof(struct node*));// vector of 16 pointers to node structures
+  struct node **hash=(struct node **)calloc(16,sizeof(struct node**));// vector of 16 pointers to node structures
   int  *sizeVector; // vector that will keep track of the doubly linked lists size
-  int option, value, index;
+  int option, value, index, result;
   sizeVector = (int*) calloc(16, sizeof(int));
 
-  menu(option);
+  menu(&option);
 
   while(option!=4){
     if(option == 1){
-      printf("What value you wanna insert: %d", &value);
+      printf("What value you wanna insert: ");
+      scanf("%d", &value);
       index = hashFunc(value);
       insert(value, hash, sizeVector, index);
     }
     else if(option == 2){
-      printf("What value you wanna remove: %d", &value);
+      printf("What value you wanna remove: ");
+      scanf("%d", &value);
       index = hashFunc(value);
-      remove(value, hash, sizeVector, index);
+      result = removeKey(value, hash, sizeVector, index);
     }
     else if(option == 3){
-      prinf("What value you wanna search: %d", &value);
+      printf("What value you wanna search: ");
+      scanf("%d", &value);
       index = hashFunc(value);
-      search(value, hash);
+      result = search(value, hash, index);
+      if(result == -1){
+        printf("Value was not found :( \n");
+      }else{
+          printf("Value %d was found \n", result);
+      }
+
     }
 
-    menu(option);
+    menu(&option);
   }
 
 
