3e077e33a816855ad34c954d4bae1358063a4246
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-16 18:57:44 -0300

    print function is working

--- homework/bubblesort.asm
+++ homework/bubblesort.asm
@@ -1,13 +1,14 @@
 .data 
 	array: .word 7,5,2,1,1,3,4 
 .align 0 
-
+	endln: .asciiz "\n"
 .text 
 
 .globl main 
 
 
 main: 
+	jal print 
 	li $t0, 7
 	li $t2, 0 
 	
@@ -36,6 +37,31 @@ main:
 				j innerLoop
 				
 	endOutLoop:
+	jal print 
+        j endSort
+        
+
+print: 
 	
+	la $s1, array
+	li $t0, 0
+	li $t1, 7
 	
+	loopPrint:
+		beq $t0,$t1, endPrint
+		addi $t0, $t0, 1 
+		lw $a0,($s1)
+		li $v0,1
+		syscall 
+		
+		addi $s1,$s1, 4
+		
+		j loopPrint
+	endPrint: 
+	li $v0, 4 
+	la $a0, endln 
+	syscall 
+	jr $ra 
+	
+endSort:
 	
